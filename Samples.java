5ffd16d0c99604f0800d3fbd47e8f5444872569f
LukEscopeta <lfcnarcizo73@gmail.com>
2025-02-24 15:45:21 -0300

    samples

---
+++ Samples.java
@@ -0,0 +1,19 @@
+
+
+public class Samples
+{
+    // atributo de classe Samples
+    int idade;
+
+    // método main
+    public static void main(String args[])
+    {
+        //variável do método main
+        int vidade = 10;
+
+        System.out.println("Alo mundo" + idade);
+        System.out.println("Alo mundo" + vidade);
+    }
+    
+
+}
